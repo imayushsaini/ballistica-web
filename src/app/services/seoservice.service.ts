a024e31f0abb85058f85ed20c4e1c3e1ee0caed4
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2023-07-16 03:08:55 +0530

    fixed dynamic canonical

--- src/app/services/seoservice.service.ts
+++ src/app/services/seoservice.service.ts
@@ -15,14 +15,14 @@ export class SEOServiceService {
   }
 
   updateOgUrl(url: string) {
-    this.meta.addTag({ name: 'og:url', content: url })
+    this.meta.updateTag({ name: 'og:url', content: url })
+    this.meta.removeTag('rel="canonical"');
+    this.meta.addTag({ rel: 'canonical', href: url });
   }
 
   updateDescription(desc: string) {
     this.meta.updateTag({ name: 'description', content: desc })
     this.meta.updateTag({ name: 'og:description', content: desc })
   }
-  updateCanonicalTag(url:string ) {
-   this.meta.addTag({ rel: 'canonical', href: url });
-  }
+
 }
