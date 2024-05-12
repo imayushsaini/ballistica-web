341454de7faea8963dfac4c0747bc7e266837e5f
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2024-05-12 22:16:42 +0530

    fix server.ts

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import 'zone.js/node';
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import * as express from 'express';
-import { existsSync } from 'node:fs';
-import { join } from 'node:path';
+import { existsSync } from 'fs';
+import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
